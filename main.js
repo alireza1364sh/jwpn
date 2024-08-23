ecce0a27f8dcae2209015a8175f8918ac1a57e2c
ali <alireazashabani1364@gmail.com>
2024-08-24 00:50:15 +0330

    a

--- main.js
+++ main.js
@@ -1,261 +1,215 @@
 const http = require('http');
 const shell = require('shelljs');
 const eURL = require('url');
-const site_server = http.createServer();
-const { networkInterfaces } = require('os');
-const logger = require('logger').createLogger("vpn.log");
-const TronWeb = require('tronweb')
-const bcrypt = require('bcrypt');
 const fs = require('fs');
-const httpPort = 7199;
-const version = 2.2;
-const resolveConfFile = "/etc/resolv.conf"
-const serverConfFile = "/etc/openvpn/server/server.conf"
-let Contract = null;
-let tronWeb = null;
-let smartAddress = "";
+const logger = require('logger').createLogger("vpn.log");
 const sleep = require('sleep-promise');
+const httpPort = 7199;
+
+const site_server = http.createServer();
 
 startHttpServer();
+
 async function startHttpServer() {
+    logger.info("HTTP server starting...");
 
-  
-    logger.info("http server start ...");
-
-    site_server.on('error', (err)=>{
-        logger.error("http server error ", err.stack);
+    site_server.on('error', (err) => {
+        logger.error("HTTP server error:", err.stack);
     });
 
-    site_server.on('request', async function (req, res) {
-
-        logger.info("*** start request", req.method);
+    site_server.on('request', async (req, res) => {
+        logger.info("Start request:", req.method);
 
         try {
-
             let U = eURL.parse(req.url, true);
-            logger.info("request info", req.method, JSON.stringify(U));
+            logger.info("Request info:", req.method, JSON.stringify(U));
 
             if (req.method === "GET") {
                 switch (U.pathname.replace(/^\/|\/$/g, '')) {
-                    case "create" :
+                    case "create":
                         await addVpn(req, res, U.query);
                         break;
-                    case "remove" :
+                    case "remove":
                         await removeVpn(req, res, U.query);
                         break;
-                     case "list" :
+                    case "list":
                         await listUser(req, res, U.query);
                         break;
-                    case "check" :
-                        await checkToken(req,res,U.query);
-                        break; 
-                        case "enableVpn":
-                          await enableVpn(req, res, U.query);
-                          break;
-                      case "disableVpn":
-                          await disableVpn(req, res, U.query);
-                          break;
-                    default :
-                        logger.info("pathname not found !", U.pathname);
+                    case "check":
+                        await checkToken(req, res, U.query);
+                        break;
+                    case "enableVpn":
+                        await enableVpn(req, res, U.query);
+                        break;
+                    case "disableVpn":
+                        await disableVpn(req, res, U.query);
+                        break;
+                    default:
+                        logger.info("Pathname not found:", U.pathname);
+                        res.write("Path not found");
                 }
             }
 
-            logger.info("*** end request");
-
-        }catch (e) {
-            logger.error("DANGER !!!! >>> in request ", e.message);
+            logger.info("End request");
+        } catch (e) {
+            logger.error("Error in request:", e.message);
+            res.write("Server error occurred");
         }
 
         res.end();
     });
 
     site_server.listen(httpPort);
-    logger.info("http server listen on " + httpPort);
+    logger.info("HTTP server listening on port " + httpPort);
 }
 
-    let privateIP ;
-    async function findIp(){
-     
-      await fs.readFile('/etc/wireguard/wg0.conf', 'utf8', (err, data) => {
-      if (err) throw err;
-    
-      const allowedIPs = [];
-    
-      const lines = data.split('\n');
-    
-      for (let i = 0; i < lines.length; i++) {
-        const line = lines[i].trim();
-    
-        if (line.startsWith('AllowedIPs')) {
-          const ips = line.substring(line.indexOf('=') + 1).trim().split(',');
-    
-          for (let j = 0; j < ips.length; j++) {
-            const ip = ips[j].trim();
-    
-            if (!allowedIPs.includes(ip)) {
-              allowedIPs.push(ip);
-            }
-          }
-        }
-      }
-        const ipv4 = allowedIPs.filter(ip => ip.includes('.'));
-        
-        for(i = 3 ; i<250;i++){
-             const ipToCheck = `10.66.66.${i}/32`;
+async function findIp() {
+    const data = fs.readFileSync('/etc/wireguard/wg0.conf', 'utf8');
+    const allowedIPs = [];
 
-        if (allowedIPs.includes(ipToCheck)) {
-          logger.info(`${ipToCheck} exists in the array.`);
-        } else {
-          logger.info(`${ipToCheck} does not exist in the array.`);
-          privateIP = i;
-          return
+    const lines = data.split('\n');
+    for (let line of lines) {
+        line = line.trim();
+        if (line.startsWith('AllowedIPs')) {
+            const ips = line.split('=')[1].trim().split(',');
+            allowedIPs.push(...ips.map(ip => ip.trim()));
         }
-        
-        
-        }
-        
-       
-                
-        
-      logger.info('here is allowip ' ,ipv4);
-    });
     }
 
+    for (let i = 3; i < 250; i++) {
+        const ipToCheck = `10.66.66.${i}/32`;
+        if (!allowedIPs.includes(ipToCheck)) {
+            logger.info(`${ipToCheck} is available.`);
+            return i;
+        }
+    }
 
-async function checkToken(req,res,query){
-    // res.write('hello');
-    let file_is_exist = await  fs.existsSync("/root/wg0-client-"+query.publicKey+".conf")
-    if (file_is_exist){
-        await res.write('true')
-    }else{
-        await res.write('false')
+    throw new Error('No available IP found');
+}
+
+async function checkToken(req, res, query) {
+    const filePath = `/root/wg0-client-${query.publicKey}.conf`;
+    if (fs.existsSync(filePath)) {
+        res.write('true');
+    } else {
+        res.write('false');
     }
 }
 
 async function addVpn(req, res, query) {
-  let myip = await findIp();
-  await sleep(2222);
-  logger.info('my ip is here', privateIP);
-  
-  let file_is_exist = await fs.existsSync("/root/wg0-client-" + query.publicKey + ".conf");
-  logger.info('1', file_is_exist);
+    const privateIP = await findIp();
+    await sleep(2222);
+    logger.info('Assigned IP:', privateIP);
 
-  if (!file_is_exist) {
-      const result = shell.exec('/home/jwpn/wireguard-install.sh', { async: true });
-      result.stdin.write('1\n'); // Enter 1
-      result.stdin.write(query.publicKey + '\n'); // Enter public key
-      result.stdin.write(privateIP + '\n'); // Enter IP
-      result.stdin.write(privateIP + '\n'); // Confirm IP
-      result.stdin.end();
+    const filePath = `/root/wg0-client-${query.publicKey}.conf`;
+    if (!fs.existsSync(filePath)) {
+        const result = shell.exec('/home/jwpn/wireguard-install.sh', { async: true });
+        result.stdin.write(`1\n${query.publicKey}\n${privateIP}\n${privateIP}\n`);
+        result.stdin.end();
 
-      await result.on('close', async () => {
-          await sleep(2000);
+        result.on('close', async () => {
+            await sleep(2000);
 
-          let filePath = "/root/wg0-client-" + query.publicKey + ".conf";
-          if (fs.existsSync(filePath)) {
-              // در اینجا تنظیمات اضافی به نتیجه اضافه می‌شوند
-              let fileContent = fs.readFileSync(filePath, 'utf8');
-              fileContent = fileContent.replace(
-                  '[Interface]',
-                  '[Interface]\nMTU = 1280'
-              );
-              fileContent = fileContent.replace(
-                  '[Peer]',
-                  '[Peer]\nPersistentKeepalive = 1'
-              );
-              fs.writeFileSync(filePath, fileContent, 'utf8');
-              
-              res.write(fileContent);
-          } else {
-              res.write('فایل پس از ایجاد موجود نیست');
-          }
-      });
+            if (fs.existsSync(filePath)) {
+                let fileContent = fs.readFileSync(filePath, 'utf8');
+                fileContent = fileContent.replace('[Interface]', '[Interface]\nMTU = 1280');
+                fileContent = fileContent.replace('[Peer]', '[Peer]\nPersistentKeepalive = 1');
+                fs.writeFileSync(filePath, fileContent, 'utf8');
 
-  } else {
-      let filePath = "/root/wg0-client-" + query.publicKey + ".conf";
-      if (fs.existsSync(filePath)) {
-          let fileContent = fs.readFileSync(filePath, 'utf8');
-          res.write(fileContent);
-      } else {
-          res.write('فایل موجود نیست');
-      }
-  }
+                res.write(fileContent);
+            } else {
+                res.write('File not found after creation');
+            }
+        });
+    } else {
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+        res.write(fileContent);
+    }
 }
 
+async function removeVpn(req, res, query) {
+    const result = shell.exec('/home/jwpn/wireguard-install.sh', { async: true });
+    result.stdin.write('3\n');
 
+    result.stdout.on('data', (data) => {
+        const regex = new RegExp(`(\\d+)\\) ${query.publicKey}`);
+        const matches = regex.exec(data.toString());
 
+        if (matches && matches[1]) {
+            const number = matches[1];
+            logger.info('Selected user number:', number);
+            result.stdin.write(`${number}\n`);
+        } else {
+            logger.info('No match found for the public key');
+        }
+    });
 
+    result.on('close', (code) => {
+        logger.info('Command exited with code:', code);
+        res.write('VPN removed.');
+    });
 
-
-async function removeVpn(req, res, query){
-
- const result = shell.exec('/home/jwpn/wireguard-install.sh', { async: true });
-
-  
-  result.stdin.write('3\n'); // Enter 1
-   
-  let selecteduser ;
-   result.stdout.on('data', (data) => {
- logger.info('Console response:', data.toString());
- 
-//  const regex = /(\d+)\) ali/;
- const regex = new RegExp(`(\\d+)\\) ${query.publicKey}`);
-const matches = regex.exec(data.toString());
-
-if (matches && matches[1]) {
-  const numberBeforeAli = parseInt(matches[1]);
-  selecteduser =  logger.info('Number before "ali":', numberBeforeAli);
-    
-  logger.info('selecteduser',parseInt(numberBeforeAli)+'\n')
-// result.stdin.write(parseInt(numberBeforeAli)+'\n'); // Enter name 'ali'
-  result.stdin.write(numberBeforeAli+'\n'); // Enter name 'ali'
-} else {
-  logger.info('No match found for the pattern');
-}
- 
- 
- 
-});
-
- 
-//   result.stdin.write(query.publicKey+'\n'); // Enter name 'ali'
-  
-logger.on('close', (code) => {
-  console.log('Command exited with code:', code);
-});
-  result.stdin.end();
+    result.stdin.end();
 }
 
+async function listUser(req, res, query) {
+    const result = shell.exec('/home/jwpn/wireguard-install.sh', { async: true });
+    result.stdin.write('2\n');
 
+    let listUserData = '';
 
+    result.stdout.on('data', (data) => {
+        listUserData += data.toString();
+    });
 
+    result.on('close', () => {
+        logger.info('List of users:', listUserData);
+        res.write(listUserData);
+    });
 
-
-async function listUser(req, res, query){
-
- const result = shell.exec('/home/jwpn/wireguard-install.sh', { async: true });
-    let _listuser ;
-  
-  result.stdin.write('2\n'); // Enter 1
-   
-     result.stdout.on('data',async (data) => {
-    _listuser =await data.toString()
-
-
- 
-});
-  
-
-  result.stdin.end();
- await sleep(2000)
-  logger.info('Console response:', _listuser);
-  await res.write(_listuser)
+    result.stdin.end();
 }
- 
 
+async function enableVpn(req, res, query) {
+    const filePath = `/root/wg0-client-${query.publicKey}.conf`;
 
+    if (fs.existsSync(filePath)) {
+        let fileContent = fs.readFileSync(filePath, 'utf8');
+        const regex = /^PrivateKey\s*=\s*(.*)$/m;
+        const match = regex.exec(fileContent);
 
+        if (match && match[1].endsWith("1")) {
+            const activeKey = match[1].slice(0, -1);
+            fileContent = fileContent.replace(match[1], activeKey);
+            fs.writeFileSync(filePath, fileContent, 'utf8');
+            logger.info("VPN configuration activated for key:", query.publicKey);
+            res.write("VPN configuration enabled.");
+        } else {
+            res.write("VPN configuration is already active.");
+        }
+    } else {
+        res.write("Configuration file not found.");
+    }
+}
 
+async function disableVpn(req, res, query) {
+    const filePath = `/root/wg0-client-${query.publicKey}.conf`;
 
+    if (fs.existsSync(filePath)) {
+        let fileContent = fs.readFileSync(filePath, 'utf8');
+        const regex = /^PrivateKey\s*=\s*(.*)$/m;
+        const match = regex.exec(fileContent);
 
-
+        if (match && !match[1].endsWith("1")) {
+            const inactiveKey = match[1] + "1";
+            fileContent = fileContent.replace(match[1], inactiveKey);
+            fs.writeFileSync(filePath, fileContent, 'utf8');
+            logger.info("VPN configuration deactivated for key:", query.publicKey);
+            res.write("VPN configuration disabled.");
+        } else {
+            res.write("VPN configuration is already inactive.");
+        }
+    } else {
+        res.write("Configuration file not found.");
+    }
+}
